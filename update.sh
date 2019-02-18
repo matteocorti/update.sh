73d26babf8249d6fe98b70ab4f184780fbd48295
Matteo Corti <matteo@corti.li>
2019-02-18 08:50:31 +0100

    Added verbose option

--- update.sh
+++ update.sh
@@ -2,14 +2,45 @@
 
 clear;
 
+VERBOSE=""
+
+error() {
+    printf 'Error: %s\n' "${1}" 1>&2
+    exit 1
+}
+
+run_command() {
+    COMMAND="$1"
+    if [ -n "${VERBOSE}" ] ; then
+	printf 'excuting: "%s"\n' "${COMMAND}"
+    fi
+    eval "${COMMAND}"
+}
+
+while true; do
+
+    case "$1" in
+        -v|--verbose)
+            VERBOSE=1
+            shift
+            ;;
+	*)
+	    if [ -n "${1}" ] ; then
+		error "invalid option: ${1}"
+	    fi
+	    break
+	    ;;
+    esac
+
+done
+
 echo "################################################################################"
 echo "# Microsoft"
 echo "#"
 echo
 
-/Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app/Contents/MacOS/msupdate --list
-
-/Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app/Contents/MacOS/msupdate --install
+run_command '/Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app/Contents/MacOS/msupdate --list'
+run_command '/Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app/Contents/MacOS/msupdate --install'
 
 echo
 echo "##############################################################################"
@@ -17,7 +48,7 @@ echo "# Apple"
 echo "#"
 echo
 
-sudo softwareupdate -ia
+run_command 'sudo softwareupdate -ia'
 
 echo
 echo "##############################################################################"
@@ -27,7 +58,7 @@ echo
 
 if command -v mas > /dev/null 2>&1 ; then
 
-    mas upgrade
+    run_command 'mas upgrade'
     
 fi
 
@@ -37,12 +68,12 @@ echo "# MacPorts"
 echo "#"
 echo
 
-sudo port selfupdate
-sudo port installed outdated
+run_command 'sudo port selfupdate'
+run_command 'sudo port installed outdated'
 
 if port installed outdated | grep -q -v 'None of the specified ports are installed.' ; then
-    sudo port -c upgrade outdated
-    sudo port -u -q uninstall
+    run_command 'sudo port -c upgrade outdated'
+    run_command 'sudo port -u -q uninstall'
 fi
 
 if  command -v brew > /dev/null 2>&1 ; then
@@ -53,8 +84,8 @@ if  command -v brew > /dev/null 2>&1 ; then
     echo "#"
     echo
     
-    brew update
-    brew upgrade
+    run_command 'brew update'
+    run_command 'brew upgrade'
 
 fi
 
@@ -71,15 +102,15 @@ if [ -f "${PERLBREW_ROOT}/etc/bashrc" ] ; then
     # shellcheck source=${HOME}/perl5/perlbrew/etc/bashrc
     . "${PERLBREW_ROOT}/etc/bashrc"
 
-    perlbrew self-upgrade
+    run_command 'perlbrew self-upgrade'
 
     for version in $( perlbrew list | sed "s/[* ]*\\([^ ]*\\).*/\\1/" ) ; do
 
-	perlbrew use "${version}"
-	perlbrew upgrade-perl   
+	run_command "perlbrew use ${version}"
+	run_command 'perlbrew upgrade-perl'
 	LIST=$( cpan-outdated -p --exclude-core )
 	if [ -n "${LIST}" ] ; then
-            cpanm $LIST
+            run_command "cpanm ${LIST}"
 	fi
     
     done
